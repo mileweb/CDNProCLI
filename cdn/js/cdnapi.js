c8cea356115e0f9633be4fe1367ea28a2b5b4d2c
Bin Ni <nibin@quantil.com>
2024-10-01 17:06:20 -0700

    Update cdnapi.js
    Fixed errors

--- cdn/js/cdnapi.js
+++ cdn/js/cdnapi.js
@@ -1,28 +1,21 @@
-const fs = require('fs');
-const qtlapi = require('./qtl-api-tools');
+const { buildAuth, callServer } = require('./qtl-api-tools');
 const { cred } = require('./SECRET_credentials');
 
 function listProperties({limit = 5, range = 'self+children', target = 'production'}) {
-    let ngOptions = qtlapi.buildAuth(cred.ngServer);
+    let ngOptions = buildAuth(cred.ngServer); //build the auth header from credential
+    // fill in the details of the API endpoint
     ngOptions.path = `/cdn/properties?target=${target}&limit=${limit}`;
     ngOptions.headers['Report-Range']=range;
     ngOptions.abortOnError = false;
-    qtlapi.callServer(ngOptions, ngProcProperties);
+    // make the call, supply the callback function
+    callServer(ngOptions, ngProcProperties);
 }
 
-function dumpJson(jsonData) {
+// the callback function, takes 2 parameters.
+// the first one is the json object of the response body
+// the second one is a context, which contains details about the API request and response
+function ngProcProperties(jsonData, ctx) {
     console.log(jsonData);
 }
 
-// process NG properties list
-let ngProcProperties = dumpJson;
-
-function volSummary({startTime = '', endTime = '', range = 'self+children'}) {
-    let ngOptions = qtlapi.buildAuth(cred.ngServer);
-    ngOptions.path = `/cdn/properties?target=${target}&limit=${limit}`;
-    ngOptions.headers['Report-Range']=range;
-    ngOptions.abortOnError = false;
-    qtlapi.callServer(ngOptions, ngProcProperties);
-}
-
-getProperties({limit:3});
+listProperties({limit:3});
