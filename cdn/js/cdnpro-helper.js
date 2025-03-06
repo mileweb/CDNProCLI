9873978e9c15c112dbdf8efdde83d1a276fcd2c6
Bin Ni <nibin@BinsMBA.local>
2025-03-05 18:52:47 -0800

    improved tool cdnPro,js

--- cdn/js/cdnpro-helper.js
+++ cdn/js/cdnpro-helper.js
@@ -389,6 +389,9 @@ function diffObjects(a, b) {
 }
 
 async function getCustomer(customerId, o) {
+    if (customerId === '--help') {
+        return {usage: 'getCustomer customerId', minArgs: 1, maxArgs: 1};
+    }
     console.log(`Getting Info of Customer ${customerId}...`);
     const options = buildAuth(null, o); // use the default server info from setServerInfo()
     options.path = `/ngadmin/customers/${customerId}`;
@@ -433,6 +436,9 @@ async function listCustomers(o) {
 }
 
 async function getPropertyVersion(id_or_domain, ver, o) {
+    if (id_or_domain === '--help') {
+        return {usage: 'getPropertyVersion id_or_domain production/staging/latest/number', minArgs: 2, maxArgs: 2};
+    }
     let isDomain = id_or_domain.indexOf('.') > 0;
     let id = id_or_domain;
     let verN = ver;
@@ -464,10 +470,14 @@ async function getPropertyVersion(id_or_domain, ver, o) {
     options.path = `/cdn/properties/${id}` + (ver ? `/versions/${verN}` : '');
     // parameters supported by this endpoint
     const customer = await callServer(options);
+    customer.obj._propertyId_ = id;
     return customer.obj;
 }
 
 function getProperty(id_or_domain, o) {
+    if (id_or_domain === '--help') {
+        return {usage: 'getProperty id_or_domain', minArgs: 1, maxArgs: 1};
+    }
     return getPropertyVersion(id_or_domain, null, o);
 }
 
@@ -483,6 +493,9 @@ async function listProperties(o) {
 }
 
 async function getServiceQuota(customerId, o) {
+    if (customerId === '--help') {
+        return {usage: 'getServiceQuota customerId', minArgs: 1, maxArgs: 1};
+    }
     if (customerId == null) {
         throw new Error('customerId is not defined');
     }
@@ -518,6 +531,9 @@ async function patchServiceQuota(serviceQuotaId, obj) {
 }
 
 async function getSystemConfigs(o) {
+    if (o === '--help') {
+        return {usage: 'getSystemConfigs', minArgs: 0, maxArgs: 0};
+    }
     console.log('Getting systemConfigs ...');
     const options = buildAuth(null, o); // use the default server info from setServerInfo()
     options.path = `/cdn/systemConfigs`;
--- cdn/js/tools/cdnPro.js
+++ cdn/js/tools/cdnPro.js
@@ -1,33 +1,48 @@
 const { buildAuth, callServer, cdnpro } = require('../cdnpro-helper');
+const path = require('path');
 const { cred } = require('../SECRET_credentials');
 
-function usage() {
-    const scriptName = process.argv[1];
-    console.log(`Usage: node ${scriptName} function data`);
-    console.log('  function: the function name to call, one of:');
+async function usage() {
+    const absPath = process.argv[1];
+    const relPath = path.relative(process.cwd(), absPath);
+    const scriptName = absPath.length <= relPath.length ? absPath : relPath;
+    console.log(`Usage:`);
     for (let f in cdnpro) {
         if (typeof cdnpro[f] === 'function' && f.startsWith('get')) {
-            console.log('    ', f);
+            let help = await cdnpro[f]('--help');
+            console.log(`node ${scriptName}`, help.usage);
         }
     }
-    console.log('  data: parameters for the function');
     console.log('Example: node', scriptName, 'getCustomer', '123');
 }
 
 async function main() {
     if (process.argv.length < 3) {
-        usage();
+        await usage();
         process.exit(1);
     }
     const func = process.argv[2];
-    const data = process.argv[3];
     if (!cdnpro[func] || typeof cdnpro[func] !== 'function' || !func.startsWith('get')) {
         console.error('Error: function', func, 'not found');
-        usage();
+        await usage();
         process.exit(1);
     }
     cdnpro.setServerInfo(cred.cdnPro);
-    const result = await cdnpro[func](data);
+    const funcHelp = await cdnpro[func]('--help');
+    let result = null;
+    if (funcHelp.minArgs == 0)
+        result = await cdnpro[func]();
+    else if (funcHelp.minArgs == 1)
+        result = await cdnpro[func](process.argv[3]);
+    else if (funcHelp.minArgs == 2)
+        result = await cdnpro[func](process.argv[3], process.argv[4]);
+    else if (funcHelp.minArgs == 3)
+        result = await cdnpro[func](process.argv[3], process.argv[4], process.argv[5]);
+    else {
+        console.error('Error: function', func, 'needs too many arguments');
+        await usage();
+        process.exit(1);
+    }
     console.log(result);
     process.exit(0);
 }
