4d82e774935af4b5a09e5671e8ed7cf7e174715b
Bin Ni <nibin@BinsMBA.local>
2025-03-24 23:42:09 -0700

    more comments for the task template

--- cdn/js/tools/batchTask.template.js
+++ cdn/js/tools/batchTask.template.js
@@ -7,8 +7,10 @@ let taskConfig = {
     batchLimit: 50, // the number of properties to process in one batch
     // a coarse filter to select candidate properties in the "find" stage
     findFilter: {hasConfig: 'origins.servers:quantil.com'},
-    // exact criteria to select the properties to update
+    // The exact criteria to select the properties to update
     // input is the property version obj, returns true or false
+    // It is required that the condition should NOT match the new property
+    // version created by createVersion() below.
     condition: function(pv) {
         // if the origin server name contains quantil.com, and directConnect is not 'auto'
         if (pv.origins == null) return false;
@@ -19,8 +21,10 @@ let taskConfig = {
         }
         return false;
     },
-    // the input is a deep copy of the original property version
-    // you can modify it directly and return it
+    // The input is a deep copy of the original property version.
+    // You can modify it directly and return as the new property version.
+    // It is required that the new property version should NOT meet the condition() above.
+    // This is to avoid the same property being processed multiple times.
     createVersion: function(nv) {
         for (o of nv.origins) {
             if (o.servers.some(s=>s.includes('quantil.com'))) {
@@ -29,6 +33,11 @@ let taskConfig = {
         }
         return nv; // return the new property version
     }
+    //This is an example to replace a directive in the edge logic
+    // createVersion: function(nv) {
+    //     nv.edgeLogic = nv.edgeLogic.replace('old_directive', 'new_directive');
+    //     return nv; // return the new property version
+    // }
 };
 
 exports.taskConfig = taskConfig;
